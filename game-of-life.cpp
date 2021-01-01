a129f56fd6a00210aee2b48f13786ef2c4a46cd6
Rupert Wu <wu.rupert@outlook.com>
2021-01-01 05:50:19 -0500

    Added display/clear loop.

--- game-of-life.cpp
+++ game-of-life.cpp
@@ -12,18 +12,31 @@
 
 using namespace std;
 
+void clear(int H) {
+    for (int i = 0; i < H; i++)
+        cout << "\e[2K\r\e[1A";
+}
+
+void display(bool **board, int H, int W) {
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++)
+            cout << board[i][j];
+        cout << "\n";
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        printf("usage: game-of-life <height> <width> <transitions> [<odds>]\n");
+    if (argc < 5) {
+        printf("usage: game-of-life <height> <width> <transitions> <rate> [<odds>]\n");
         exit(1);
     }
 
     int H = atoi(argv[1]), W = atoi(argv[2]);
     int T = atoi(argv[3]);
-    (void)T;
+    int R = atoi(argv[4]);
     int O = 12;
-    if (argc == 5)
-        O = atoi(argv[4]);
+    if (argc == 6)
+        O = atoi(argv[5]);
 
     time_t timer;
     srand((unsigned)time(&timer));
@@ -37,7 +50,13 @@ int main(int argc, char *argv[]) {
     bool *board_rows[H];
     for (int i = 0; i < H; i++)
         board_rows[i] = board[i];
-    (void)board_rows;
+
+    display(board_rows, H, W);
+    for (int t = 0; t < T; t++) {
+        sleep(R);
+        clear(H);
+        display(board_rows, H, W);
+    }
 
     return 0;
 }
